3faae10187697bc3f67ccb45c207bbbc383aad5e
LeoGarzon <leogarzons90@gmail.com>
2023-08-05 18:17:46 -0500

    first commit

--- main.js
+++ main.js
@@ -1,18 +1,24 @@
-const COTIZACION = 10000000;
-const cotizarEquipos = (equipos) => equipos / COTIZACION;
-const cotizarAsesoria = (asesoria) => asesoria / COTIZACION;
-let seleccion = prompt("SELECCIONAR COTIZACION \n 1 - EQUIPOS \n 2 -  ASESORIA ");
-let valor = prompt("VALOR");
-switch (seleccion) {
-    case "1":
-        alert(cotizarEquipos(valor));
-        break;
-    case "2":
-        alert(cotizarEquipos(valor));
-        break;
-    default:
-        break;
-       
+const Servicios = function(nombre,precio) {
+    this.nombre=nombre;
+    this.precio=precio;
+}
+
+let servicios1 = new Servicios("asesoria", 1200000)
+let servicios2 = new Servicios("equipos", 2500000)
+let servicios3 = new Servicios("instalacion", 3000000)
+let servicios4 = new Servicios("soporte tecnico", 1500000)
+
+let lista = [servicios1,servicios2,servicios3,servicios4]
+
+function filtrarServicios(){
+    let palabraClave = prompt("Ingrese servicio a cotizar")
+    let resultado = lista.filter((servicio)=> servicio.nombre.includes(palabraClave))
+
+    if(resultado.length > 0){
+        console.table(resultado)
+    }else{
+        alert("sin resultado" + palabraClave)
+    }
 }
 
 
